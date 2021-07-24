ff37f6f4b3588c2f0b0ddddb6383220928b1a694
Kona <kitami.kitano20@gmail.com>
2021-07-24 11:40:03 +0900

    Django Webアプリ開発実装ハンドブック 7章 accountsアプリの作成、マイグレーションの実行まで

---
+++ photoProject/photo/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+# URLパターンを逆引きできるように名前をつける
+app_name = 'photo'
+
+# URLパターンを登録するための変数
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    # path('', views.IndexView.as_view(), name='index'),
+]
