1b030d22cd807f3405586511263d3e0c2ac665c4
Kona <kitami.kitano20@gmail.com>
2021-07-28 23:53:12 +0900

    photoProject TemplateViewでの変数の渡し方参考

--- photoProject/photo/views.py
+++ photoProject/photo/views.py
@@ -4,3 +4,8 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        page_context = super().get_context_data(**kwargs)
+        page_context["page"] = "main"
+        return page_context
