3007dc2e5e3d698539fa626aa06b478695f79a94
Kona <kitami.kitano20@gmail.com>
2021-08-10 23:55:12 +0900

    createsuperuserでアカウント作成

--- photoProject/accounts/admin.py
+++ photoProject/accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    '''管理ページのレコード一覧に表示するカラムを設定するクラス
+    '''
+    # レコード一覧にidとusernameを表示
+    list_display = ('id', 'username')
+    # 表示するカラムにリンクを設定
+    list_display_links = ('id', 'username')
+
+
+# 管理サイトにCustomUser, CustomUserAdminを登録
+admin.site.register(CustomUser, CustomUserAdmin)
