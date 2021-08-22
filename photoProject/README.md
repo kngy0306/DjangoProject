480ce29472e20687e684e23909c02b113f4a1ce2
Kona <kitami.kitano20@gmail.com>
2021-08-22 15:47:26 +0900

    ログイン、ログアウト機能実装

---
+++ photoProject/README.md
@@ -0,0 +1,9 @@
+## photoProject
+
+/admin  
+admin  
+12345678
+
+/login  
+kona1  
+yoshinama123
--- photoProject/accounts/urls.py
+++ photoProject/accounts/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'accounts'
 
 urlpatterns = [
+    # viewsモジュールのsignUpViewをインスタンス化する
     path('signup/', views.SignUpView.as_view(), name='signup'),
+    # viewsモジュールのsignUpSuccessViewをインスタンス化する
     path('signup_success', views.SignUpSuccessView.as_view(), name='signup_success'),
+    # ログイン django.contrib.auth.views.LoginViewをインスタンス化する
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    # ログアウト django.contrib.auth.views.logoutViewをインスタンス化する
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
